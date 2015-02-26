9239749f7dfb13799f6def3d7b009fdc8a807a14
Itay Weinberger <itay@joo.la>
2015-02-26 12:09:32 +0200

    #1 initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var
+  config = require('config'),
+  Githook = require('./lib/githook');
+
+var githook = new Githook({
+
+}, function (err, details) {
+  console.log('Joobot is listening at http://%s:%s', details.host, details.port);
+  githook.on('push', function (eventdata) {
+    //console.log(eventdata);
+  });
+  githook.on('pull_request', function (eventdata) {
+    console.log(eventdata);
+  });
+});
